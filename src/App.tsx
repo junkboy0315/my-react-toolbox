c0a00cea83ebcbc7b5ad9e845a75df75126959d3
Shota Tamura <tamura@yuuniworks.com>
2020-09-03 10:24:20 +0900

    animate children

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import HeroSlick from './components/HeroSlick';
+import gsap from 'gsap';
+import React, { forwardRef, useCallback, useEffect, useRef } from 'react';
+
+const Title = forwardRef<HTMLDivElement, { name: string }>((props, ref) => {
+  const { name } = props;
+  return (
+    <div>
+      <div>
+        <div ref={ref}>name is: {name}</div>
+      </div>
+    </div>
+  );
+});
+
+const AnimatedTitles = ({ names }: { names: Array<string> }) => {
+  const nameRefs = useRef<Array<HTMLDivElement>>([]);
+
+  const onRefUpdate = useCallback((el: any, index: number) => {
+    nameRefs.current[index] = el;
+  }, []);
+
+  useEffect(() => {
+    gsap.from(nameRefs.current, 1, {
+      x: 100,
+      stagger: 0.2,
+    });
+  }, []);
+
+  return (
+    <div>
+      {names.map((name, index) => {
+        return <Title name={name} ref={(el) => onRefUpdate(el, index)} />;
+      })}
+    </div>
+  );
+};
 
 function App() {
-  return (
-    <HeroSlick
-      srcs={[
-        'https://hikimiaoiya.com/wp/wp-content/themes/aoiya/assets/images/main/mv_01.jpg',
-        'https://hikimiaoiya.com/wp/wp-content/themes/aoiya/assets/images/main/mv_02.jpg',
-      ]}
-    />
-  );
+  const names = ['dog', 'cat', 'bird'];
+
+  return <AnimatedTitles names={names} />;
 }
 
 export default App;
