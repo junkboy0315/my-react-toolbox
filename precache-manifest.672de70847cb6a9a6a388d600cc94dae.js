4e3a16abe688f49468fe63bb8f8b8cb9ae800dbe
junkboy0315 <junkboy0315@users.noreply.github.com>
2020-09-10 02:23:37 +0000

    deploy: 11aaf36ae11954fbdeb6d4e654ad57e98ca7e667

---
+++ precache-manifest.672de70847cb6a9a6a388d600cc94dae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e282295fbbd42658e980fe241a72be52",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.65be70bd1771f162203d.bundle.js"
+  },
+  {
+    "url": "runtime~main.65be70bd1771f162203d.bundle.js"
+  },
+  {
+    "revision": "845d0f409221f47215ed",
+    "url": "static/css/main.40af7ae7.chunk.css"
+  },
+  {
+    "url": "vendors~main.65be70bd1771f162203d.bundle.js"
+  },
+  {
+    "url": "vendors~main.65be70bd1771f162203d.bundle.js.LICENSE.txt"
+  }
+]);
